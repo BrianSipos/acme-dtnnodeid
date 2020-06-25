228c5ed1c37fe034f90450d526c9d2ad22c251a1
Brian Sipos <bsipos@rkf-eng.com>
2020-06-24 21:07:17 -0400

    Copied in artwork helper

---
+++ spec/split_artwork.py
@@ -0,0 +1,41 @@
+''' Split artwork from an RFC XML file and feed them to a command.
+'''
+import os
+import sys
+import logging
+import argparse
+import subprocess
+import lxml.etree as etree
+
+LOGGER = logging.getLogger()
+
+
+def main():
+    ''' Main entry for the tool. '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log-level', default='info',
+                        help='The minimum logging severity displayed.')
+    parser.add_argument('infile', help='The file to read from.')
+    parser.add_argument('xpath', help='The XPath expression to match.')
+    parser.add_argument('command', nargs=argparse.REMAINDER,
+                        help='The command and its arguments to run and pass to stdin.')
+
+    args = parser.parse_args()
+    logging.basicConfig(level=args.log_level.upper())
+    logging.root.debug('command args: %s', args)
+
+    with open(args.infile, 'rb') as infile:
+        xml_parser = etree.XMLParser()
+        doc = etree.parse(infile, xml_parser)
+    
+    exitcodes = []
+    for elem in doc.xpath(args.xpath):
+        proc = subprocess.Popen(args.command, stdin=subprocess.PIPE)
+        proc.communicate(elem.text.encode('utf8'))
+        proc.stdin.close()
+        exitcodes.append(proc.wait())
+    
+    return max(exitcodes)
+
+if __name__ == '__main__':
+    sys.exit(main())
